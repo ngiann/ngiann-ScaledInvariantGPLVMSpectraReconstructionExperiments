50fb486712e2f9e42366e5d1121c4e679b6f31a5
Nikos Gianniotis <ngiann@gmail.com>
2025-05-19 10:35:03 +0200

    remove old data

---
+++ prepare_duck_dataset.jl
@@ -0,0 +1,28 @@
+let
+        
+    rng = MersenneTwister(1);
+
+    #################
+    # prepare data #
+    #################
+
+    C   = Diagonal(rand(rng, 62)*2.5 .+ 0.5);
+
+    ducks = loadcoil20(;every=8)[1]; 
+
+    tr_indices = sort(randperm(rng, 72)[1:62])
+    te_indices = sort(setdiff(1:72, tr_indices))
+
+    Y     = ducks[:,tr_indices] # Y is 256 × 62, i.e. 62 images of 16×16 pixels
+    Ytest = ducks[:,te_indices] 
+
+    σ = var(Y);
+
+    Sobs = (σ/10)*randn(size(Y))*C;
+
+    Yobs = Y*C + Sobs
+
+
+    JLD2.save("duck_dataset.jld2","Y",Y,"Ytest",Ytest,"Sobs",Sobs,"Yobs",Yobs,"tr_indices",tr_indices,"te_indices",te_indices)
+
+end
--- runme_scaled_coildata.jl
+++ runme_scaled_coildata.jl
@@ -1,30 +1,6 @@
 # load coil dataset
 using ScaledInvariantGPLVMSpectraReconstruction, COIL20, Random, LinearAlgebra, JLD2, Statistics
 
-rng = MersenneTwister(1);
-
-
-#################
-# prepare data #
-#################
-
-C   = Diagonal(rand(rng, 62)*2.5 .+ 0.5);
-
-ducks = loadcoil20(;every=8)[1]; 
-
-tr_indices = sort(randperm(rng, 72)[1:62])
-te_indices = sort(setdiff(1:72, tr_indices))
-
-Y     = ducks[:,tr_indices] # Y is 256 × 62, i.e. 62 images of 16×16 pixels
-Ytest = ducks[:,te_indices] 
-
-σ = var(Y);
-
-S = (σ/10)*randn(size(Y));
-
-Yobs = Y*C + S*C
-
-
 ##########
 # warmup #
 ##########
