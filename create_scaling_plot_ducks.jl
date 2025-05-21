67f9847faa187da6627ab45d77cf58236f70ad2f
Nikos Gianniotis <ngiann@gmail.com>
2025-05-21 11:51:32 +0200

    plot for recovered scalings for duck dataset

--- create_scaling_plot_ducks.jl
+++ create_scaling_plot_ducks.jl
@@ -8,8 +8,9 @@ function create_scaling_plot_ducks()
     cgplvm = JLD2.load("scaleinv_gplvm_scaled_coil_3D.jld2")["c"]
     cppca  = JLD2.load("ppca_scaled_coil_3D.jld2")["c"]
  
-    plot(legendfontsize = 17, legend = :topleft, tickfontsize = 14)
+    plot(legendfontsize = 22, guidefont = 22, legend = :topleft, tickfontsize = 14)
     scatter!(ctrue, cgplvm, label = "scale invariant GPLVM", markersize=11, color=:cyan)
     scatter!(ctrue, cppca, label = "scale invariant PPCA", markersize=11, color=:red)
-
+    xlabel!("True scaling")
+    ylabel!("Recovered scaling")
 end
