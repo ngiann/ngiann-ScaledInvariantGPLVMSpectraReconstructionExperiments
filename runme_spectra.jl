d16a5c2b65b1a200b9b7fe11dd53eb1e25d86e50
Nikos Gianniotis <ngiann@gmail.com>
2025-05-21 11:51:53 +0200

    new file for training spectra

---
+++ runme_spectra.jl
@@ -0,0 +1,72 @@
+# load coil dataset
+using ScaledInvariantGPLVMSpectraReconstruction, Random, LinearAlgebra, JLD2, Statistics
+using ScaledInvariantGPLVMSpectraReconstructionSpectralData
+using Printf
+
+##########
+# warmup #
+##########
+
+function split_training_testing_spectra_data()
+
+    _λ, f, σ = loadspectra()
+
+    # replaces NaNs with Infs
+    f[isnan.(f)] .= Inf;
+    σ[isnan.(σ)] .= Inf;
+
+    # shufle dataset just to make sure that there is no particular ordering
+    rng = MersenneTwister(13)
+    new_indices = randperm(rng, 1185)
+    f = f[:,new_indices]
+    σ = σ[:,new_indices]
+
+    @printf("Returning:\n");
+    @printf("1st argument: training 500×1000 fluxes\n")
+    @printf("2nd argument: training 500×1000 std errors\n")
+    @printf("3rd argument: testing 500×185 fluxes\n")
+    @printf("4th argument: testing 500×185 standard errors.\n")
+
+    f_tr = f[1:2:end, 1:1000]
+    σ_tr = σ[1:2:end, 1:1000]
+    f_te = f[1:2:end, 1001:1185]
+    σ_te = σ[1:2:end, 1001:1185]
+
+    return f_tr, σ_tr, f_te, σ_te
+
+end
+
+let 
+    local f_tr, σ_tr, f_te, σ_te = split_training_testing_spectra_data()
+    scaleinvariantgplvm(f_tr[:,1:10], σ_tr[:,1:10], iterations=3, Q=2)
+    gplvm(f_tr[:,1:10], σ_tr[:,1:10], iterations = 3, Q = 2)
+    ppca(f_tr[:,1:10], σ_tr[:,1:10], iterations = 3, Q = 2) 
+end
+
+
+################
+# train models #
+################
+
+# train GPLVM and save results
+function train_GPLVM_on_spectra(;seed::Int64 = seed)
+    local filename = @sprintf("gplvm_spectra_seed=%d.jld2", seed)
+    @printf("Will save result in file |%s|\n\n", filename)
+    local f_tr, σ_tr, = split_training_testing_spectra_data()
+    local X,rec,res,net,fmin, = gplvm(f_tr, σ_tr, iterations = 50_000, Q = 3, H = 30, seed = seed);
+    JLD2.save(filename, "X", X, "rec", rec, "res", res, "net", net, "fmin", fmin)
+end
+
+# # run scale-invariance GPLVM and save results
+# let
+#     Yobs, Sobs = JLD2.load("scaled_duck_dataset.jld2", "Yobs", "Sobs")
+#     X,rec,res,net,fmin,c,cvar = scaleinvariantgplvm(Yobs, Sobs, iterations = 30_000, Q=3, H = 30, seed=1);
+#     JLD2.save("scaleinv_gplvm_scaled_coil_3D.jld2", "X", X, "rec", rec, "res", res, "net", net, "fmin", fmin, "c", c, "cvar", cvar)
+# end
+
+# # run scale-invariance PPCA and save results
+# let
+#     Yobs, Sobs = JLD2.load("scaled_duck_dataset.jld2", "Yobs", "Sobs")
+#     X, res, rec, _, fmin, c = ppca(Yobs, Sobs; iterations = 30_000, Q=3, seed = 1)
+#     JLD2.save("ppca_scaled_coil_3D.jld2", "X", X, "rec", rec, "res", res, "fmin", fmin, "c", c)
+# end
