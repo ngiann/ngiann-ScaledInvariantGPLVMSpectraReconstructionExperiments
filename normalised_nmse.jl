b5a95667d44a9fba22cc4af150be489307a6e04b
Nikos Gianniotis <ngiann@gmail.com>
2025-05-28 21:30:49 +0200

    introduce nmse

---
+++ normalised_nmse.jl
@@ -0,0 +1,11 @@
+# ŷ is the prediction
+# y is the test data item
+function normalised_nmse(ŷ, y)
+
+    aux1 = mean(abs2.(ŷ - y))
+
+    aux2 = mean(abs2.(y .- mean(y)))
+
+    aux1 / aux2
+
+end
